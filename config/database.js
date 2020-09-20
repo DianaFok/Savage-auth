1be10263a2e85a36aa93eea94b4a78f690f832f5
Diana Fok <diana.y.fok@gmail.com>
2020-09-20 16:42:48 -0400

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbuser2:w3i4oqnhpJxpRi5p@cluster0.gkntz.mongodb.net/<dbname>?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
